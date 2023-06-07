53f8f47c88ae7f581ec13d84db6149170b87e2aa
Jonny Ingall <jonny@mondago.com>
2023-06-07 10:58:58 +0100

    Make guessing work

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,61 @@
 use leptos::*;
 use rand::seq::IteratorRandom;
-use gloo_net::http::Request;
 
-async fn get_target_word() -> Result<String, Box<dyn std::error::Error>> {
-    let resp = Request::get("https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt")
+async fn get_target_word() -> String {
+    let resp = reqwasm::http::Request::get("https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt")
         .send()
-        .await?
-        .text()
-        .await?;
-    let lines = resp.lines().map(|c| c);
+        .await
+        .unwrap();
+    let text = resp.text().await.unwrap();
+    let lines = text.lines().map(|c| c);
     let word = lines.choose(&mut rand::thread_rng()).expect("No lines");
-    Ok(word.to_string())
+    word.to_string()
 }
 
 fn main() {
     leptos::mount_to_body(|cx| view! { cx, <App /> });
 }
 
-#[component]
-fn App(cx: Scope) -> impl IntoView {
-    let (word, set_word) = create_signal(cx, "".to_string());
-    let (guesses, set_guesses) = create_signal(cx, Vec::new());
-
-    wasm_bindgen_futures::spawn_local(async move { 
-        let target_word = get_target_word().await.expect("Failed to fetch word");
-        set_word(target_word.clone());
-        log!("{}", &target_word);
-    });
-
-    view! { cx,
-        <h1>"Absurdle"</h1>
-        <h1>{word.get()}</h1>
-        <Show
-            when=move || match guesses().last().cloned() {
-                Some(w) => w == word(),
-                None => false
-            }
-            fallback=|cx| view! {cx, <span></span>}
-        >
-            <p>"You win!"</p>
-        </Show>
-        <ErrorBoundary
-            fallback=|cx, errors| view! { cx,
-                <div style="border: 1px solid red">
-                    <p>"Errors: "</p>
-                    <ul>
-                        {move || errors.get()
-                            .into_iter()
-                            .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
-                            .collect::<Vec<_>>()
-                        }
-                    </ul>
-                </div>
-            }
-        >
-            <For
-                each=guesses
-                key=|guess| String::from(guess)
-                view=move |cx, guess| { view! { cx, <Row guess /> } }
-            />
-            <Guess set_guesses=set_guesses />
-        </ErrorBoundary>
+fn check_guess_against_word(cx: Scope, last_guess: String, target_word: Resource<(), String>) -> bool {
+    match target_word.read(cx) {
+        Some(w) => last_guess == w,
+        None => false
     }
 }
 
 #[component]
-fn Guess(cx: Scope, set_guesses: WriteSignal<Vec<String>>) -> impl IntoView {
+fn App(cx: Scope) -> impl IntoView {
+    let (guesses, set_guesses) = create_signal(cx, Vec::new());
+    let (last_guess, set_last_guess) = create_signal(cx, "".to_string());
+
+    let word = create_local_resource(cx, || (), |_| get_target_word());
+
+    let has_won = move || check_guess_against_word(cx, last_guess(), word);
+
+    let word_view = move || {
+        view! {cx, <p>{word.read(cx)}</p>}
+    };
+
+    view! { cx,
+        <h1>"Absurdle"</h1>
+        {word_view}
+        <Show
+            when=has_won
+            fallback=|cx| view! { cx, <p></p> }
+        >
+            <p>"You won!"</p>
+        </Show>
+        <For
+            each=guesses
+            key=|guess| String::from(guess)
+            view=move |cx, guess| { view! { cx, <Row guess /> } }
+        />
+        <Guess set_guesses=set_guesses set_last_guess=set_last_guess />
+    }
+}
+
+#[component]
+fn Guess(cx: Scope, set_last_guess: WriteSignal<String>, set_guesses: WriteSignal<Vec<String>>) -> impl IntoView {
     let (guess, set_guess) = create_signal(cx, "".to_string());
     let disabled = move || guess().len() != 5;
 
@@ -75,6 +65,7 @@ fn Guess(cx: Scope, set_guesses: WriteSignal<Vec<String>>) -> impl IntoView {
                 set_guesses.update(move |guesses| {
                     guesses.push(guess());
                 });
+                set_last_guess(guess());
                 set_guess("".to_string());
             }
         >
